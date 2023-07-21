11b71473009555877147de3fc24c3fc27cb71912
Nadiia <nadink1603@gmail.com>
2023-07-21 14:11:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HW3_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3, b = 7, c = 9;
+
+            Console.WriteLine(Calculate(a, b, c));
+
+            Console.ReadLine();
+        }
+
+        static string Calculate(double a, double b, double c)
+        {
+            return $"{a / 5}, {b / 5}, {c / 5}";
+        }
+    }
+}
